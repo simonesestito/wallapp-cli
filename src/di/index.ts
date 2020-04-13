7b5e8861d96da1caacd84ce084c7b31170223174
Simone Sestito <simone@simonesestito.com>
2020-04-13 23:48:14 +0200

    Create CrudRepository interface

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,11 +1,15 @@
 import { Container } from 'inversify';
 import { Firestore } from '@google-cloud/firestore';
 import { firestore, Scaleway } from '../datasource';
+import { WallpaperRepository } from '../repository';
 
 const container = new Container();
 
 // Datasources
 container.bind(Firestore).toConstantValue(firestore);
 container.bind(Scaleway).toSelf();
+
+// Repositories
+container.bind(WallpaperRepository).toSelf();
 
 export { container };
---
+++ src/repository/crud.ts
@@ -0,0 +1,44 @@
+/**
+ * Interface defining a CRUD repository
+ * It has to be implemented by every repository
+ *
+ * If an implementation has to deal with multiple entities,
+ * it should be a Manager instead of a Repository.
+ * Managers are intended more high-level classes.
+ *
+ * An implementation has to specify two generic types:
+ * - ID: the ID of the model
+ * - T: the model interface itself
+ *
+ * All methods are considered async and return Promise
+ */
+export interface CrudRepository<ID, T> {
+    /**
+     * Insert or update an entity
+     *
+     * @param data The data to insert, with the ID
+     */
+    insert(data: T): Promise<void>
+
+    /**
+     * Get an entity by its ID
+     *
+     * @param id ID to look for
+     * @return Found entity, or null
+     */
+    get(id: ID): Promise<T | null>
+
+    /**
+      * Get all entities
+      *
+      * @return All saved entities
+      */
+    getAll(): Promise<T[]>
+
+    /**
+     * Remove a specified entity by its ID
+     *
+     * @param id ID to delete
+     */
+    delete(id: ID): Promise<void>
+}
